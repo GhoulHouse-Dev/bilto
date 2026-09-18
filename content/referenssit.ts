6be85ccbf5c3d492df72e4deae4838a83a98b3c3
GhoulHouse-Dev <hello@ghoulhouse.fi>
2026-09-18 11:48:04 +0300

    Next.js redesign: add content/referenssit.ts

---
+++ content/referenssit.ts
@@ -0,0 +1,8 @@
+export const references=[
+{name:'Kisakallion Urheiluopisto',city:'Lohja'},
+{name:'Kauppakeskus Kamppi',city:'Helsinki'},
+{name:'REDI Kalasatama',city:'Helsinki'},
+{name:'Tapiola liikekeskus',city:'Espoo'},
+{name:'Musiikkitalo',city:'Helsinki'},
+{name:'Pasilan Postikeskus',city:'Helsinki'}
+] as const;
